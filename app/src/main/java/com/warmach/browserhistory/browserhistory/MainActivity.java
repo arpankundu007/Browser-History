d90d668283e304e783c95d4e429c0f6dfa7037fe
WarMach <arpankundu007@gmail.com>
2016-07-04 23:02:03 +0530

    Code clean

--- app/src/main/java/com/warmach/browserhistory/browserhistory/MainActivity.java
+++ app/src/main/java/com/warmach/browserhistory/browserhistory/MainActivity.java
@@ -82,10 +82,7 @@ public class MainActivity extends Activity {
 
     public static String getDate(long milliSeconds, String dateFormat)
     {
-        // Create a DateFormatter object for displaying date in specified format.
         SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
-
-        // Create a calendar object that will convert the date and time value in milliseconds to date.
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(milliSeconds);
         return formatter.format(calendar.getTime());
